867287fb018dd753638f7d75f9b4bedca761e4c9
dogpotato123 <jsu373712@naver.com>
2019-10-04 13:15:19 +0900

    엔티티 클래스 개발1

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class MemberRepositoryTest {
 
+    /*
     @Autowired MemberRepository memberRepository;
 
     @Test
@@ -34,4 +35,5 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
     }
+    */
 }
