0a00c97ebc2f81b82eaf161f9945d4e750f4c39f
GrandYoshi <dogpotato123@gmail.com>
2019-10-16 20:18:26 +0900

    상품도메인 개발

---
+++ src/main/java/jpabook/jpashop/Service/ItemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.Repository.ItemRepository;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
